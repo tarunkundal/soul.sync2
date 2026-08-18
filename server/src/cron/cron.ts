cc0aa28aa66cba6dcaecd3daa9aa1bd104e4db02
Tarun Chauhan <chauhan271singh@gmail.com>
2026-08-18 22:59:01 +0530

    changes the cron time

--- server/src/cron/cron.ts
+++ server/src/cron/cron.ts
@@ -1,7 +1,7 @@
 import cron from "node-cron";
 import { sendTodayEventMessages } from "../ai/sendTodayEventMessages.js";
 
-cron.schedule("2 19 * * *", async () => {
+cron.schedule("2 4 * * *", async () => {
     await sendTodayEventMessages()
 },
     { timezone: "Asia/Kolkata", }
